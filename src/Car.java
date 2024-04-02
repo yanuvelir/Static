eb67f912b5c9207906a75b92c030051e0fe938c5
yanuvelir <yanuvelir@gmail.com>
2024-04-02 21:35:00 +0300

    edited

--- src/Car.java
+++ src/Car.java
@@ -3,6 +3,13 @@ public class Car {
     private String make;
     private String model;
 
+    static int amount = 0;
+
+    public Car(String mk, String md){
+        this.make = mk;
+        this.model = md;
+    }
+
     public String getMake() {
         return make;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import javax.xml.transform.Result;
+
 public class Main {
 
     public  static void main(String[] args){
 
+        Car c1 = new Car("Honda", "Accord");
+
+        //c1.mk
+        System.out.println(c1.getMake() +" "+ c1.getModel());
         System.out.println("*Static*");
+
+        Car.amount = 1;
+        System.out.println(Car.amount);
+
     }
 }
+
+//    var n1 = 123;
+//    int Result =  n1 %10;
+//    var n2 = Result ++;
+//        System.out.println("* " + Result + " *");
